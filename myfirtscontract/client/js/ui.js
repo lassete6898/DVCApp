276e4332c0546308df7b956ab93b93942692e8d8
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2022-04-03 20:10:23 +0200

    clean up

--- myfirtscontract/client/js/ui.js
+++ myfirtscontract/client/js/ui.js
@@ -35,13 +35,15 @@ window.onload = function () {
 };
 
 async function testsito() {
+  var amount = document.getElementById("amount").value*1000000000000000000;
+  var parsedAmount = "0x" + amount.toString(16);
   await ethereum.request({
     method: "eth_sendTransaction",
     params: [
       {
         from: accounts[0],
         to: "0xCF79BE0d8a782D6C88322Db56A6A54229E29EDc3",
-        value: "0x29a2241af62c0000",
+        value: parsedAmount,
         gasPrice: "0x09184e72a000",
         // gas: '0x2710',
       },
