e9fb4dec3906edaf44fd4a09deaf9ca49accca45
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2022-05-02 19:48:27 +0200

    Update index.js

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    swal("AVISO DE COOKIES", "Utilizamos cookies propias y de terceros para mejorar nuestros servicios. Si continúa con la navegación consideraremos que acepta este uso.");
+};
+
 function login() {
     var username = document.getElementById('username').value;
 
