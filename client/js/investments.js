c3e3e72837f224cd1009dc3d2a95fffd47248cba
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2022-05-17 11:29:04 +0200

    user profile

--- client/js/investments.js
+++ client/js/investments.js
@@ -3,3 +3,18 @@ function invest(index) {
     document.getElementById("startupsSelect").options.selectedIndex = index;
     document.getElementById("startupsSelect").style.borderColor = "#007BFF";
 }
+
+function edit() {
+    Swal.fire({
+        title: '<h1> Editar información </h1>',
+        icon: 'info',
+        html:
+          '<input class="form-control my-2" type="text" placeholder="Ingrese su nombre">' +
+          '<input class="form-control" type="text" placeholder="Ingrese su apellido">' +
+          '<input class="form-control my-2" type="email" placeholder="Ingrese su correo">' +
+          '<a href="./passwordReset.html" class="link-primary" style="cursor:pointer;"> Cambiar contraseña </a>',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false
+    })
+}
